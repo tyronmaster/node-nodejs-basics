ac3b587b13823ac914ba2a8359c56a84c298aa83
tyronmaster <degonski.art@gmail.com>
2023-05-28 15:37:58 +0300

    feat: implement rename file task

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const rename = async () => {
-    // Write your code here 
+    const FILE_PATH = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(FILE_PATH);
+    const SOURCE_DIR = 'files';
+    const currentFileName = 'wrongFilename.txt';
+    const newFileName = 'properFilename.md';
+    const ERROR_MESSAGE = 'FS operation failed';
+
+    fs.readdir(path.join(__dirname, SOURCE_DIR), { withFileTypes: false }, (err, files) => {
+        if (err) throw new Error(ERROR_MESSAGE);
+        if (!files.includes(currentFileName) || files.includes(newFileName)) throw new Error(ERROR_MESSAGE);
+
+        fs.rename(path.join(__dirname, SOURCE_DIR, currentFileName), path.join(__dirname, SOURCE_DIR, newFileName), (err) => {
+            if (err) throw new Error(ERROR_MESSAGE);
+            console.log(`File ${currentFileName} successfully renamed to ${newFileName}`);
+        })
+    })
 };
 
 await rename();
