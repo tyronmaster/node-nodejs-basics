b777365670f65e8a952d0e2b0ec5fc80c91b7a61
tyronmaster <degonski.art@gmail.com>
2023-05-28 22:34:23 +0300

    feat: implements env parsing task

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,10 @@
+import process from 'process';
+
 const parseEnv = () => {
-    // Write your code here 
+    const params = process.env;
+    const keys = Object.keys(params).filter(key => key.includes('RSS_'));
+
+    keys.forEach(key => console.log(`${key}=${params[key]}`));
 };
 
 parseEnv();
