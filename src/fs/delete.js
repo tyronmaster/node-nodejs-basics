71e9dbf283750c729e7a2778e6b85bc79ea0b133
tyronmaster <degonski.art@gmail.com>
2023-05-28 19:44:03 +0300

    feat: implement file delete task

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,23 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const remove = async () => {
-    // Write your code here 
+    const FILE_PATH = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(FILE_PATH);
+    const ERROR_MESSAGE = 'FS operation failed';
+    const FILE_NAME = 'fileToRemove.txt';
+    const dirPath = path.join(__dirname, 'files')
+    const filePath = path.join(dirPath, FILE_NAME);
+
+    fs.readdir(dirPath, (err, files) => {
+        if (err) throw new Error(ERROR_MESSAGE);
+        if (!files.includes(FILE_NAME)) throw new Error(ERROR_MESSAGE);
+        fs.unlink(filePath, (err) => {
+            if (err) throw new Error(ERROR_MESSAGE);
+            console.log(`File ${FILE_NAME} deleted`);
+        });
+    })
 };
 
 await remove();
