67a6b4d1450ba0be7ec4c675c48b05347732ece7
tyronmaster <degonski.art@gmail.com>
2023-05-28 19:52:03 +0300

    feat: implement files list task

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,17 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const list = async () => {
-    // Write your code here 
+    const ERROR_MESSAGE = 'FS operation failed';
+    const DIR_PATH = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(DIR_PATH);
+
+    fs.readdir(path.join(__dirname, 'files'), (err, files) => {
+        if (err) throw new Error(ERROR_MESSAGE);
+        console.log(files);
+    })
+
 };
 
 await list();
