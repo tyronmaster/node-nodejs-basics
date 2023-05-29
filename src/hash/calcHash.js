e4db7964649556105533e95e573075e8868ba642
tyronmaster <degonski.art@gmail.com>
2023-05-29 16:16:09 +0300

    feat: implement hash task

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import crypto from 'crypto';
+import { fileURLToPath } from 'url';
+
 const calculateHash = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const hash = crypto.createHash('sha256');
+
+    fs.readFile(path.join(__dirname,'files', 'fileToCalculateHashFor.txt'), 'utf-8', (err, data) => {
+        if(err) throw new Error('Error when read the file');
+        let code = hash.update(data);
+        console.log(code.digest('hex'));
+    })
 };
 
 await calculateHash();
