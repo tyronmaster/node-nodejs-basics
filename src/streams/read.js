c8cb18b3abb725c5ad2c0e48a7cf8d053b18111b
tyronmaster <degonski.art@gmail.com>
2023-05-29 17:17:02 +0300

    feat: implement read stream task

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,24 @@
+import fs from 'fs';
+import path from 'path';
+import process from 'process';
+import { fileURLToPath } from 'url';
+import { stdin, stdout } from 'process';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+    const fileToReadPath = path.join(__dirname, 'files', 'fileToRead.txt');
+
+    const stream = fs.createReadStream(fileToReadPath, 'utf-8');
+    stream.on('data', (chunk) => {
+        // add \n to imitate console.log
+        process.stdout.write(chunk + '\n');
+    });
+    stream.on('error', (err)=>{
+        process.stdout.write('File read error' + '\n');
+    })
+
+    // Write your code here
 };
 
 await read();
