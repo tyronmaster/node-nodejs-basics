e6793fd814825519a615588c0e0f533a539612f3
tyronmaster <degonski.art@gmail.com>
2023-06-08 13:14:39 +0300

    feat: implement child process task

--- src/cp/cp.js
+++ src/cp/cp.js
@@ -1,6 +1,21 @@
+import { spawn } from 'child_process';
+import path from 'path';
+import fs from 'fs';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const pathToChild = path.join(__dirname, 'files', 'script.js');
+
 const spawnChildProcess = async (args) => {
-    // Write your code here
+    const child = spawn('node', args, {
+        stdio: ['inherit', 'inherit', 'inherit', 'ipc'],
+    });
+
+    child.on('data', (data) => {
+        console.log(data.toString());
+    });
 };
 
 // Put your arguments in function call to test this functionality
-spawnChildProcess( /* [someArgument1, someArgument2, ...] */);
+spawnChildProcess([pathToChild, 'test2', 'test3', 'close']);
